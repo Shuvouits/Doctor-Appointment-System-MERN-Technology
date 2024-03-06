0885995f42e6c89c28fda19fbf48c52f63de33aa
Shuvo Bhowmik <shuvobhowmik_cse11uits@yahoo.com>
2024-03-06 13:49:14 +0600

    Testmonial css design issue solved

--- client/src/components/Testmonial.jsx
+++ client/src/components/Testmonial.jsx
@@ -14,14 +14,14 @@ const TestimonialCarousel = ({ review }) => {
   const [selectedItem, setSelectedItem] = useState(0);
 
   const testimonialData = review.map((item, index) => ({
-    id: index + 1, 
+    id: index + 1,
     name: item.fullName,
     email: item.email,
     time: item.time,
-    comment: item.message, 
-    image: item.avatar, 
+    comment: item.message,
+    image: item.avatar,
     rating: item.ratingNumber,
-    
+
   }));
 
   const settings = {
@@ -56,30 +56,36 @@ const TestimonialCarousel = ({ review }) => {
       <div className="testimonial-carousel-container">
         <Slider {...settings}>
           {testimonialData.map((testimonial, index) => (
-            <div key={testimonial.id} className={`testimonial-item ${selectedItem + 1 === index ? 'selected' : ''}`}>
-              <div className='first'>
-                <div className='img'>
-                  <img src={testimonial.image} alt={testimonial.name} />
+            <div className='row'>
+
+              <div key={testimonial.id} className={`testimonial-item ${selectedItem + 1 === index ? 'selected' : ''}`}>
+                <div className='first'>
+                  <div className='img'>
+                    <img src={testimonial.image} alt={testimonial.name} />
+                  </div>
+                  <div className='ninfo'>
+                    <span className="name">{testimonial.name}</span>
+                    <span>{format(testimonial.time, 'dd-MMMM-yyyy')}</span>
+                    <span className="rating">
+                      {Array.from({ length: 5 }, (_, i) => (
+                        <FaStar key={i} className={i < testimonial.rating ? 'icon-test filled' : 'icon-test empty'} />
+                      ))}
+                    </span>
+                  </div>
                 </div>
-                <div className='ninfo'>
-                  <span className="name">{testimonial.name}</span>
-                  <span>{format(testimonial.time, 'dd-MMMM-yyyy')}</span>
-                  <span className="rating">
-                    {Array.from({ length: 5 }, (_, i) => (
-                      <FaStar key={i} className={i < testimonial.rating ? 'icon-test filled' : 'icon-test empty'} />
-                    ))}
-                  </span>
+                <div className='second'>
+                  <p style={{flexWrap: 'wrap'}}>{testimonial.comment}</p>
                 </div>
               </div>
-              <div className='second'>
-                <p>{testimonial.comment}</p>
-              </div>
+
             </div>
+
           ))}
 
 
         </Slider>
       </div>
+
     </div>
   );
 };
